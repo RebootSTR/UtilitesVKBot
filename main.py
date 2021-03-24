91b7d830c00ca2c602734e62f2cd6842f1ea9fe7
Рафиков Айдар Робертович <190210@edu.psuti.ru>
2021-03-25 01:00:52 +0400

    deleted skip_list

--- main.py
+++ main.py
@@ -32,16 +32,12 @@ def parse_messages():
         if update[0] == 4:  # СООБЩЕНИЕ
             # print(update)
             message = Message(update)
-            if message.get_id() in SKIP_LIST:
-                SKIP_LIST.remove(message.get_id())
-                continue
             if Command.is_command(message):
-                SKIP_LIST.extend(Command.execute(message, vk))
+                Command.execute(message, vk)
             print(message.toString())
 
 
 BASE_NAME = "base.db"
-SKIP_LIST = []
 
 if __name__ == '__main__':
     base = DataBase(BASE_NAME)
