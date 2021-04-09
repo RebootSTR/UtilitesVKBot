3984327cca6064aa594eca8f1b0a3f29451d3a8b
Рафиков Айдар Робертович <190210@edu.psuti.ru>
2021-04-09 16:18:41 +0400

    fix

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     start_time = int(time.time())
     while True:
         os.system(f"python bot.py -start_time {start_time}")
-        # os.system("git pull")
+        os.system("git pull")
