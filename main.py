3bf3b6f7c14a96cef846f7fd72b331374565681b
Рафиков Айдар Робертович <190210@edu.psuti.ru>
2021-04-09 16:19:01 +0400

    fix again))

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ import time
 if __name__ == '__main__':
     start_time = int(time.time())
     while True:
-        os.system(f"python bot.py -start_time {start_time}")
+        os.system(f"python3 bot.py -start_time {start_time}")
         os.system("git pull")
